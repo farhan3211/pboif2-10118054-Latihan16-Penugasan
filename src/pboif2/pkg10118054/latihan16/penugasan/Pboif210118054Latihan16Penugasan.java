f44aeb2aa6743e8c341cf60ccbae8b2e65024206
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:28:46 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan16/penugasan/Pboif210118054Latihan16Penugasan.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan16.penugasan;
+
+/**
+ *
+ * @author ASUS
+ * nama   : Farhan Milardi
+ * kelas  :IF-2
+ * nim    :10118054
+ */
+public class Pboif210118054Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     * int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        
+        c += a;
+        System.out.println("c += a = " + c);
+        
+        c -= a;
+        System.out.println("c -= a = " + c);
+        
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = "  + c);
+        c >>= 2;
+        System.out.println("c >>= a = "  + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
